e4d462c6c1a99ba76bed343a1e42f2f1584d28ff
Parimal Mahindrakar <parimalm4653@gmail.com>
2021-09-21 13:35:22 +0530

    Added files

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-import json
+import json, datetime
 
 import pymysql
 
@@ -13,9 +13,11 @@ cursor = conn.cursor()
 
 customers = {}
 orders = {}
+orderitems = {}
 custnorders = {
     "customers" : customers,
     "orders" : orders,
+    "ordersitems" : orderitems
 }
 
 
@@ -24,7 +26,7 @@ def selectall():
     data = cursor.fetchall()
     for i in data:
         customers.update({i[0] : i[1:]})
-    cursor.execute("select orderid, ordername, quantity, name, orderedOn, status from Customer join Orders where Orders.custid = Customer.custid")
+    cursor.execute("select orderid, quantity, OrderItemName, orderedOn, status, name from Customer, OrderItem, Orders where Customer.custid=Orders.custid and Orders.orderitemid = OrderItem.orderitemID")
     data = cursor.fetchall()
     for i in data:
         orders.update({i[0] : i[1:]})
@@ -33,6 +35,11 @@ def selectall():
     data = cursor.fetchall()[0][0]
     custnorders.update({"pending" : data})
     custnorders.update({"nonpending" : len(orders) - data })
+    cursor.execute('select * from OrderItem')
+    data = cursor.fetchall()
+    for i in data:
+        orderitems.update({i[0] : i[1:]})
+
 
 
 
@@ -41,12 +48,128 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     selectall()
+    # print(custnorders)
     return render_template("index.html",data = custnorders)
 
 @app.route("/addcust",  methods =["GET", "POST"])
 def addcustomer():
     if request.method == "POST":
         data = json.loads(request.data)
+        name = data['name']
+        gender = "F" if(data['gender'] == "Female") else "M"
+        phone = data['phone']
+        address = data['address']
+        query = f'insert into Customer(name, gender, phone, address) values("{name}", "{gender}","{phone}", "{address}");'
+        cursor.execute(query)
+        conn.commit()
         print(data)
+        return jsonify(data)
     return "FORBIDDEN"
 
+@app.route("/addorder",  methods =["GET", "POST"])
+def addorder():
+    if request.method == "POST":
+        data = json.loads(request.data)
+        custid = data['custid']
+        orderitemid = data['orderitemid']
+        qtyorderitem = data['qtyorderitem']
+        info = checkqty(qtyorderitem, orderitemid)
+        print(info)
+        if(info[0] > 0):
+            d = datetime.datetime.now()
+            print(int(qtyorderitem)*info[1])
+            datenew = str(d.year)+"-"+str(d.month)+"-"+str(d.day)
+            query = f'insert into Orders(quantity, custid, orderitemid, orderedOn, amount) values({qtyorderitem}, {custid}, {orderitemid},"{datenew}", {int(qtyorderitem)*info[1]})'
+            cursor.execute(query)
+            conn.commit()
+            print("updated stock ",info)
+            query = f'update OrderItem set stock = {info[0]} where orderitemID = {orderitemid}'
+            cursor.execute(query)
+            conn.commit()
+            return jsonify({"msg" : "QE"}) # query executed
+        return jsonify({"msg" : "SE"}) # stock exceeded
+    return "FORBIDDEN"
+        
+
+def checkqty(qty,item):
+    cursor.execute(f"select stock, price from OrderItem where orderitemID = {item}")
+    data = cursor.fetchall()
+    return [(int(data[0][0]) - int(qty)), int(data[0][1]) ]
+    # return True if int(qty) < int(data) else False
+
+
+@app.route('/searchcust',  methods =["GET", "POST"])
+def searchcustomer():
+    if request.method == "POST":
+        data = json.loads(request.data)['words']
+        cursor.execute(f"select * from Customer where name  like '%{data}%'")
+        data = cursor.fetchall()
+        return jsonify({"msg" : data})
+
+@app.route('/searchorder',  methods =["GET", "POST"])
+def searchorder():
+    if request.method == "POST":
+        data = json.loads(request.data)['words']
+        cursor.execute(f"select orderid, quantity, OrderItemName, orderedOn, status, name from Customer, OrderItem, Orders where Customer.custid=Orders.custid and Orders.orderitemid = OrderItem.orderitemID and name like '%{data}%'")
+        data = list(cursor.fetchall())
+        
+        for i in data:
+            i =  list(i)
+            # i[3] = str(i[3].year)+"-"+str(i[3].month)+"-"+str(i[3].day)
+        print(data)
+        return jsonify({"msg" : data})
+
+
+
+
+@app.route("/getpendingntotal", methods =["GET", "POST"])
+def getpending():
+    if request.method == "POST":
+        data =  json.loads(request.data)['whatwewant']
+        query = ""
+        if data == "Pending" or data == "Completed" : 
+            query = f'select orderid, quantity, OrderItemName, orderedOn, status, name from Customer, OrderItem, Orders where Customer.custid=Orders.custid and Orders.orderitemid = OrderItem.orderitemID and status = "{data}";'
+        else :
+            query = f'select orderid, quantity, OrderItemName, orderedOn, status, name from Customer, OrderItem, Orders where Customer.custid=Orders.custid and Orders.orderitemid = OrderItem.orderitemID;'
+
+        cursor.execute(query)
+        data = cursor.fetchall()
+        return jsonify(data)
+
+'''
+
+    create table Customer (
+        custid int primary key auto_increment,
+        name varchar(50) not null,
+        gender varchar(1) not null,
+        phone varchar(10) not null,
+        address varchar(100) not null default "Pune"
+    );
+
+    create table OrderItem (
+        orderitemID int primary key auto_increment, 
+        OrderItemName varchar(50), 
+        stock int check (stock > 0)  ,
+        price int
+    );
+
+    create table Orders(     
+        orderid int primary key auto_increment,      
+        quantity int,      
+        custid int,      
+        orderitemid int,
+        status varchar(30) default "Pending",     
+        orderedOn date,   
+        amount int,  
+        foreign key(custid) references Customer(custid),
+        foreign key(orderitemid) references OrderItem(orderitemID)
+    );
+
+    insert into OrderItem(OrderItemName, stock, price) values
+    ("Samsung TV", 300, 40000),
+    ("Samsung G M30S", 780, 14500),
+    ("Realme 5G", 400, 19500),
+    ("Lenovo Ideapad 330", 170, 42040),
+    ("Xiomi TV", 900,25000);
+
+'''
--- FlaskApp/static/js/app.js
+++ FlaskApp/static/js/app.js
@@ -14,15 +14,167 @@ $("i.fa").click(() => {
 
 
 $("input#addcustbutton").click(() => {
-    var name = $("input#flname")
-    var phone = $("input#phonenum")
-    var address = $("input#address")
 
+    var name = $("input#flname").val()
+    var phone = $("input#phonenum").val()
+    var address = $("input#address").val()
+    var gender = $("select#genders").val()
+    
     var url = "/addcust"
-    // fetch(url, )
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            'name': name,
+            'phone': phone,
+            'address': address,
+            'gender' : gender
+        })
+    }).then((res) => {
+        return res.json()
+    }).then((data) => {
+        console.log(data)
+        window.location.reload()
+    })
 
+})
+
+$("input#addorderbutton").click(() => {
+
+    var custid = $("select#custselect").val()
+    var orderitemid = $("select#orderitemselect").val()
+    var qtyorderitem = $("input#qtyorderitem").val()
+
+    var url = "/addorder"
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            'custid': custid,
+            'orderitemid': orderitemid,
+            'qtyorderitem' : qtyorderitem
+        })
+    }).then(res => res.json()).then((data) => {
+        if (data.msg == "SE") {
+            alert("Stock Exceeded or Stock Over")
+        } else {
+            window.location.reload()
+        }
+        
+    })
+})
+
+
+$("input#searchcust").keyup(() => {
+    var words = $("input#searchcust").val()
+    if (words.length > 2) {
+        fetch('/searchcust', {
+            method: 'POST',
+            body: JSON.stringify({
+                'words' :words
+            })
+        }).then((res) => {
+            return res.json()
+        }).then((data) => {
+    
+            $("div.all__customers").empty()
+           
+    
+            for (var i = 0; i < data.msg.length; i++) {
+                var id = data.msg[i][0]
+                var name = data.msg[i][1]
+                var gender = data.msg[i][2]
+                var phone = data.msg[i][3]
+                var address = data.msg[i][4]
+    
+                console.log(id, " ", name, " ", phone, " ", address)
+                
+                str = '<div class="customers__infos">\
+                    <div><p>'+id+'</p></div>\
+                    <div><p><a href="#"></a>'+name+' ('+gender+') </p></div>\
+                    <div><p>'+phone+'</p></div>\
+                    <div><p>'+address+'</p></div>\
+                </div>'
+                $("div.all__customers").append(str)
+            }
+    
+    
+        })
+    } else if(words.length == 0){
+        location.reload()
+    }
+    
+})
+
+
+$("input#searchorderid").keyup(() => {
+    var words = $("input#searchorderid").val()
+    if (words.length > 2) {
+        
+        fetch("/searchorder", {
+            method: "POST",
+            body: JSON.stringify({
+                'words' :words
+            })
+        }).then(res => res.json()).then((data) => {
+
+            
+            $("div.all__orders").empty()
+            
+            for (var i = 0; i < data.msg.length; i++) {
+                orderid = data.msg[i][0]
+                orderqty = data.msg[i][1]
+                ordername = data.msg[i][2]
+                date = data.msg[i][3]
+                status = data.msg[i][4]
+                orderedby = data.msg[i][5]
+
+
+                str = '<div class="orders__infos">\
+                    <div><p>'+ orderid +'</p></div>\
+                    <div><p>'+ordername+' - ('+orderqty+') </p></div>\
+                    <div><p> '+date+' </p></div>\
+                    <div><p>'+status+'</p></div>\
+                    <div><p>'+orderedby+'</p></div>\
+                </div>'
+
+                $("div.all__orders").append(str)
+
+            }
+
+
+        })
+
+
+    } else if(words.length == 0){
+        location.reload()
+    }
 })
 
 
 
+$("div#pendingid").click(() => {
+    fetch("/getpendingntotal", {
+        method: "POST",
+        body: JSON.stringify({
+            'whatwewant' : "Pending"
+        })
+    })
+})
+
+$("div#totalid").click(() => {
+    fetch("/getpendingntotal", {
+        method: "POST",
+        body: JSON.stringify({
+            'whatwewant' : "all"
+        })
+    })
+})
+
+$("div#completedid").click(() => {
+    fetch("/getpendingntotal", {
+        method: "POST",
+        body: JSON.stringify({
+            'whatwewant' : "Completed"
+        })
+    })
+})
 
